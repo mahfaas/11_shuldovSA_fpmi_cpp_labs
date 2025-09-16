f841475e09278d4344e01dfe429f19213e371dd5
mahfaas <stasstassta364@gmail.com>
2025-09-16 09:31:29 +0300

    Added v10.cpp

---
+++ semester_1/lab2_integer_arithmetics/v10.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main() 
+{
+    long long a, b;
+    std::cout << "Enter a and b: " << std::endl;
+    if(!(std::cin >> a >> b) || a < 0 || b < 0 || a > b)
+    {
+        std::cout << "Error!" << std::endl;
+        return 0;
+    }
+    long long cur = 1; 
+    int n = 1;
+    bool f = false;
+    while(true) 
+    {
+        if(cur >= a && cur <= b) 
+        {
+            std::cout << cur << " ";
+            f = true;
+        }
+        cur = 2 * cur + 1;
+        n++;
+        if(cur > b) 
+            break;
+    }
+    if(!f) 
+        std::cout << "No Mersenne numbers in the range(" << std::endl;
+    return 0;
+}
